2988ba50b245eb6189ca404dbb5cd638a1b2a472
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-05-09 17:39:11 +0530

    Bug: Fixed scrolling issue on nav-menu & code improvements

--- js/app.js
+++ js/app.js
@@ -2,21 +2,24 @@ const navMenu = document.getElementById('nav-menu');
 const ham = document.getElementById('ham');
 const content = document.getElementById('content');
 
-content.addEventListener('touchstart', () => {
-    visible = navMenu.getAttribute('toggle');
+visible = navMenu.getAttribute('toggle');
 
+// Hide the navigation menu if the user touches any area outside of nav-menu i.e, the blurred content
+content.addEventListener('touchstart', () => {
     if (visible == 'true') {
         navMenu.setAttribute('toggle', false);
+        document.body.style.overflow = "auto";
     }
 });
 
-ham.addEventListener('click', () => {
-    visible = navMenu.getAttribute('toggle');
-
+// Add functionality to the hamburger for hiding & unhiding the navigation
+// Setting the appropriate overflow property on the body to lock scrolling when menu is open
+ham.addEventListener('touchstart', () => {
     if (visible == 'false') {
         navMenu.setAttribute('toggle', true);
+        document.body.style.overflow = "hidden";
     } else {
         navMenu.setAttribute('toggle', false);
-
+        document.body.style.overflow = "auto";
     }
 });

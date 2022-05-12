0fb9b6acec42f1c6c410c25729015c6a098f819b
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-05-12 14:00:11 +0530

    Bug: Fixed phantom animation of navigation on window resize
    Thanks to: @terril-dactyl
    https://github.com/terril-dactyl/portfolio-v1/commit/422046918a8f198c8945f2fc858f911bba85a659

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
+// Navigation
 const navMenu = document.getElementById('nav-menu');
 const ham = document.getElementById('ham');
 const content = document.getElementById('content');
 
 // Hide the navigation menu if the user touches any area outside of nav-menu i.e, the blurred content
+// Add transition to make the nav menu slide out. JS is used to add this to avoid phantom animation playing on window resize
+// Rmeove the transition as soon as it has completed. Timeout will wait until the 1st event i.e, transition has completed
 content.addEventListener('click', () => {
     visible = navMenu.getAttribute('toggle');
 
     if (visible == 'true') {
         navMenu.setAttribute('toggle', false);
+        navMenu.style.transition = 'transform 200ms ease-in';
+        setTimeout(() => {
+            navMenu.style.removeProperty('transition');
+        });
         document.body.style.overflow = "auto";
     }
 });
@@ -22,6 +29,10 @@ ham.addEventListener('click', () => {
         document.body.style.overflow = "hidden";
     } else {
         navMenu.setAttribute('toggle', false);
+        navMenu.style.transition = 'transform 200ms ease-in';
+        timeoutID = setTimeout(() => {
+            navMenu.style.removeProperty('transition');
+        });
         document.body.style.overflow = "auto";
     }
 });

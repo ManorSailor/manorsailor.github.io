5575d67348e7e2818bd0adce04f9f7f62fec9897
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-05-13 18:59:43 +0530

    Organise JS code & minor improvements

--- js/app.js
+++ js/app.js
@@ -1,31 +1,43 @@
-// Disable all transitions & animations while the window is being resized. This helps in avoiding phantom animations
-// Timeout of 0.5s, just for a little breathing room if the user resizes their window like a maniac
-// Store the last timeout in a variable & clear it when another timeout is set. This isn't required, just a good practice to clear what you've initialized.
+/* ================= Variables/Constants ================= */
 let oldTimeout;
 
+/* ================= Navbar Constants ================= */
+const navMenu = document.getElementById('nav-menu');
+const ham = document.getElementById('ham');
+const content = document.getElementById('content');
+
+/* ================== Global ==================== */
+
+// Disable transitions & animations globally while the window is being resized. This helps in avoiding phantom animations
 window.addEventListener('resize', () => {
     document.body.classList.add('no-animations');
 
-    clearTimeout(oldTimeout);
+    // If width is greater than 768px, force close the navbar
+    if (window.innerWidth > 768) {
+        navMenu.setAttribute('toggle', false);
+    }
 
+    // Clear the oldtimeout. Not required, just a good practice to clear what you've initialized
+    clearTimeout(oldTimeout);
+    
+    // Timeout of 0.5s, just for a little breathing room if the user resizes their window like a maniac
     oldTimeout = setTimeout(() => {
         document.body.classList.remove('no-animations');
     }, 500);
 });
 
-// Navigation
-const navMenu = document.getElementById('nav-menu');
-const ham = document.getElementById('ham');
-const content = document.getElementById('content');
+/* ================= Navigation ================= */
 
 // Hide the navigation menu if the user touches any area outside of nav-menu i.e, the blurred content
-content.addEventListener('click', () => {
-    visible = navMenu.getAttribute('toggle');
-
-    if (visible == 'true') {
-        navMenu.setAttribute('toggle', false);
-        document.body.style.overflow = "auto";
-    }
+['click', 'touchmove'].forEach(eve => {
+    content.addEventListener(eve, () => {
+        visible = navMenu.getAttribute('toggle');
+    
+        if (visible == 'true') {
+            navMenu.setAttribute('toggle', false);
+            document.body.style.overflow = "auto";
+        }
+    });
 });
 
 // Add functionality to the hamburger for hiding & unhiding the navigation

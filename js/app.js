b9fce9a308248188f812fd508760d2b5a92e9acb
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-05-13 16:53:13 +0530

    Bug: Fixed phantom transitions & introduced a global solution to avoid similar problems in the future
    Big thanks to:
    @terril-dactyl for guiding me in the right direction
    @kevin-powell for helping out with phantom animations on pseudo elements

--- js/app.js
+++ js/app.js
@@ -1,20 +1,29 @@
+// Disable all transitions & animations while the window is being resized. This helps in avoiding phantom animations
+// Timeout of 0.5s, just for a little breathing room if the user resizes their window like a maniac
+// Store the last timeout in a variable & clear it when another timeout is set. This isn't required, just a good practice to clear what you've initialized.
+let oldTimeout;
+
+window.addEventListener('resize', () => {
+    document.body.classList.add('no-animations');
+
+    clearTimeout(oldTimeout);
+
+    oldTimeout = setTimeout(() => {
+        document.body.classList.remove('no-animations');
+    }, 500);
+});
+
 // Navigation
 const navMenu = document.getElementById('nav-menu');
 const ham = document.getElementById('ham');
 const content = document.getElementById('content');
 
 // Hide the navigation menu if the user touches any area outside of nav-menu i.e, the blurred content
-// Add transition to make the nav menu slide out. JS is used to add this to avoid phantom animation playing on window resize
-// Rmeove the transition as soon as it has completed. Timeout will wait until the 1st event i.e, transition has completed
 content.addEventListener('click', () => {
     visible = navMenu.getAttribute('toggle');
 
     if (visible == 'true') {
         navMenu.setAttribute('toggle', false);
-        navMenu.style.transition = 'transform 200ms ease-in';
-        setTimeout(() => {
-            navMenu.style.removeProperty('transition');
-        });
         document.body.style.overflow = "auto";
     }
 });
@@ -29,10 +38,6 @@ ham.addEventListener('click', () => {
         document.body.style.overflow = "hidden";
     } else {
         navMenu.setAttribute('toggle', false);
-        navMenu.style.transition = 'transform 200ms ease-in';
-        timeoutID = setTimeout(() => {
-            navMenu.style.removeProperty('transition');
-        });
         document.body.style.overflow = "auto";
     }
 });
